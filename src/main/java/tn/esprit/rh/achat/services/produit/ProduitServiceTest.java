a8f94659cf548702d19c2d93902cfc80b1e96947
sarra19 <sarra.benhamida@esprit.tn>
2024-03-03 11:09:29 +0100

    first commit

---
+++ src/main/java/tn/esprit/rh/achat/services/produit/ProduitServiceTest.java
@@ -0,0 +1,15 @@
+package tn.esprit.rh.achat.services.produit;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class ProduitServiceTest {
+
+    @Test
+    public void testSomething() {
+
+        assertEquals(2, 1 + 1);
+    }
+}
